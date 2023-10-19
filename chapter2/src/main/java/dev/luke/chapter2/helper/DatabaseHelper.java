f999ee57009d58b4e3ebce60a40f2668d09309e6
Luke Wang <me@lukewang.dev>
2023-10-19 14:12:14 +0800

    refactor(Chapter 2): move database code to DatabaseHelper.java

---
+++ chapter2/src/main/java/dev/luke/chapter2/helper/DatabaseHelper.java
@@ -0,0 +1,53 @@
+package dev.luke.chapter2.helper;
+
+import dev.luke.chapter2.service.CustomerService;
+import dev.luke.chapter2.util.PropsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseHelper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseHelper.class);
+    private static final String DRIVER;
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
+
+    static {
+        Properties conf = PropsUtil.loadProperties("config.properties");
+        DRIVER = conf.getProperty("jdbc.driver");
+        URL = conf.getProperty("jdbc.url");
+        USERNAME = conf.getProperty("jdbc.username");
+        PASSWORD = conf.getProperty("jdbc.password");
+
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("can not load jdbc driver", e);
+        }
+    }
+
+    public static Connection openConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            LOGGER.error("get connection failure.", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                LOGGER.error("close connection failure", e);
+            }
+        }
+    }
+
+}
--- chapter2/src/main/java/dev/luke/chapter2/service/CustomerService.java
+++ chapter2/src/main/java/dev/luke/chapter2/service/CustomerService.java
@@ -1,5 +1,6 @@
 package dev.luke.chapter2.service;
 
+import dev.luke.chapter2.helper.DatabaseHelper;
 import dev.luke.chapter2.model.Customer;
 import dev.luke.chapter2.util.PropsUtil;
 import org.slf4j.Logger;
@@ -13,30 +14,12 @@ import java.util.Properties;
 public class CustomerService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
-    private static final String DRIVER;
-    private static final String URL;
-    private static final String USERNAME;
-    private static final String PASSWORD;
-
-    static {
-        Properties conf = PropsUtil.loadProperties("config.properties");
-        DRIVER = conf.getProperty("jdbc.driver");
-        URL = conf.getProperty("jdbc.url");
-        USERNAME = conf.getProperty("jdbc.username");
-        PASSWORD = conf.getProperty("jdbc.password");
-
-        try {
-            Class.forName(DRIVER);
-        } catch (ClassNotFoundException e) {
-            LOGGER.error("can not load jdbc driver", e);
-        }
-    }
 
     public List<Customer> getCustomerList() {
         List<Customer> customers = new ArrayList<>();
         String sql = "SELECT id,name,contact,telephone,email,remark FROM customer";
-        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-             PreparedStatement statement = connection.prepareStatement(sql);
+        Connection connection = DatabaseHelper.openConnection();
+        try (PreparedStatement statement = connection.prepareStatement(sql);
              ResultSet rs = statement.executeQuery()
         ) {
             while (rs.next()) {
@@ -51,6 +34,8 @@ public class CustomerService {
             }
         } catch (SQLException e) {
             LOGGER.error("execute sql failure.", e);
+        } finally {
+            DatabaseHelper.closeConnection(connection);
         }
         return customers;
     }
