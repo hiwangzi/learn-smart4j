5f144d271d8e3b25f11a2e32c9a68cc42b497865
Luke Wang <me@lukewang.dev>
2023-10-19 14:35:12 +0800

    refactor(Chapter 2): use commons-dbutils to simplify database code

--- chapter2/src/main/java/dev/luke/chapter2/helper/DatabaseHelper.java
+++ chapter2/src/main/java/dev/luke/chapter2/helper/DatabaseHelper.java
@@ -1,16 +1,20 @@
 package dev.luke.chapter2.helper;
 
-import dev.luke.chapter2.service.CustomerService;
 import dev.luke.chapter2.util.PropsUtil;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Properties;
 
 public class DatabaseHelper {
+    private static final ThreadLocal<Connection> CONNECTION_THREAD_LOCAL = new ThreadLocal<>();
+    private static final QueryRunner QUERY_RUNNER = new QueryRunner();
     private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseHelper.class);
     private static final String DRIVER;
     private static final String URL;
@@ -32,22 +36,44 @@ public class DatabaseHelper {
     }
 
     public static Connection openConnection() {
-        try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            LOGGER.error("get connection failure.", e);
-            throw new RuntimeException(e);
+        Connection connection = CONNECTION_THREAD_LOCAL.get();
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (SQLException e) {
+                LOGGER.error("get connection failure.", e);
+                throw new RuntimeException(e);
+            } finally {
+                CONNECTION_THREAD_LOCAL.set(connection);
+            }
         }
+        return connection;
     }
 
-    public static void closeConnection(Connection connection) {
+    public static void closeConnection() {
+        Connection connection = CONNECTION_THREAD_LOCAL.get();
         if (connection != null) {
             try {
                 connection.close();
             } catch (SQLException e) {
                 LOGGER.error("close connection failure", e);
+            } finally {
+                CONNECTION_THREAD_LOCAL.remove();
             }
         }
     }
 
+    public static <T> List<T> queryEntityList(Class<T> entityClass, String sql, Object... params) {
+        List<T> entityList;
+        try {
+            Connection connection = openConnection();
+            entityList = QUERY_RUNNER.query(connection, sql, new BeanListHandler<T>(entityClass), params);
+        } catch (SQLException e) {
+            LOGGER.error("query entity list failure.", e);
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection();
+        }
+        return entityList;
+    }
 }
--- chapter2/src/main/java/dev/luke/chapter2/service/CustomerService.java
+++ chapter2/src/main/java/dev/luke/chapter2/service/CustomerService.java
@@ -2,41 +2,17 @@ package dev.luke.chapter2.service;
 
 import dev.luke.chapter2.helper.DatabaseHelper;
 import dev.luke.chapter2.model.Customer;
-import dev.luke.chapter2.util.PropsUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 public class CustomerService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
 
     public List<Customer> getCustomerList() {
-        List<Customer> customers = new ArrayList<>();
         String sql = "SELECT id,name,contact,telephone,email,remark FROM customer";
-        Connection connection = DatabaseHelper.openConnection();
-        try (PreparedStatement statement = connection.prepareStatement(sql);
-             ResultSet rs = statement.executeQuery()
-        ) {
-            while (rs.next()) {
-                Customer customer = new Customer();
-                customer.setId(rs.getLong("id"));
-                customer.setName(rs.getString("name"));
-                customer.setContact(rs.getString("contact"));
-                customer.setTelephone(rs.getString("telephone"));
-                customer.setEmail(rs.getString("email"));
-                customer.setRemark(rs.getString("remark"));
-                customers.add(customer);
-            }
-        } catch (SQLException e) {
-            LOGGER.error("execute sql failure.", e);
-        } finally {
-            DatabaseHelper.closeConnection(connection);
-        }
-        return customers;
+        return DatabaseHelper.queryEntityList(Customer.class, sql);
     }
 }
